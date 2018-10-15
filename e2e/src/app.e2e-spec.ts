4ce245697d99e88022510deffdfd7ed44db90c55
Vu Nguyen <vunguyen5127@gmail.com>
2018-10-16 04:31:29 +0700

    add new code

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,30 @@
-import { AppPage } from './app.po';
+import { LetslearnPage } from './app.po';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe('letslearn App', () => {
+  let page: LetslearnPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new LetslearnPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display Title Letslearn', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to letslearn!');
+    expect(page.getTitleText()).toEqual('Letslearn');
+  });
+
+  it('should click three times and reset with matching points', () => {
+    page.navigateTo();
+
+    expect(page.getPoints()).toBe('1');
+
+    page.getPlus1Button().click();
+    page.getPlus1Button().click();
+    page.getPlus1Button().click();
+
+    expect(page.getPoints()).toBe('4');
+
+    page.getResetButton().click();
+
+    expect(page.getPoints()).toBe('0');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -10,22 +11,30 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }));
 
-  it(`should have as title 'letslearn'`, () => {
+  it(`should have as title 'Letslearn'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('letslearn');
-  });
+    expect(app.title).toEqual('Letslearn');
+  }));
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to letslearn!');
-  });
+    expect(compiled.querySelector('h1').textContent).toContain('Letslearn');
+  }));
+
+  it('should increase points by 1 if button clicked', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    expect(fixture.componentInstance.points).toBe(1);
+    fixture.debugElement.nativeElement.querySelector('button').click();
+    expect(fixture.componentInstance.points).toBe(2);
+  }));
 });
